857c50d3c9de1cdc74aebacee20a97bbb76fdd18
Mihaszki <pawelmichalski9@gmail.com>
2020-12-23 15:04:13 +0100

    Add weather_api_config.json to store  api key

--- simple-weather-app-backend/app.js
+++ simple-weather-app-backend/app.js
@@ -1,11 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+// You can get api key from openweathermap.org
+const { api_key } = require('./weather_api_config.json');
 
 app.get('/', (req, res) => {
-  res.send('It works!')
+    res.send('It works!');
+})
+
+app.get('/location/:location/', (req, res) => {
+    res.send('Got it!');
+    console.log(api_key);
+    console.log(req.params.location);
 })
 
 app.listen(port, () => {
-  console.log(`simple-weater-app-backend listening at port: ${port}`)
+    console.log(`simple-weater-app-backend listening at port: ${port}`);
 })
